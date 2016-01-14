60d3f3807ce7c46c11cbb390d2e336046b484765
Josefina Araya <araya.tapia.j@gmail.com>
2016-01-14 17:00:03 -0300

    Autolote

---
+++ Models/Tipos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AutoLote.Models
+{
+    public class Tipos
+    {
+        [Key]
+        public int TipoId{ get; set; }
+        [Required]
+        public String Descripcion{ get; set; }
+        public String Comentario{ get; set; }
+
+    }
+}
